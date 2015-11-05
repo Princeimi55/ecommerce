7b7c017f0804b40c7609d5747cb24d5417c1cfe9
emallove-cayan <emallove@cayan.com>
2015-11-05 14:59:32 -0500

    TPM-646: Update the php example to use the latest CayanCheckout script.

--- examples/php/index.php
+++ examples/php/index.php
@@ -38,7 +38,7 @@
         
     <script src="http://code.jquery.com/jquery-1.11.3.min.js" type="text/javascript"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="https://ps1.merchantware.net/ecommerce/scripts/cayan-0.1.0.js" type="text/javascript"></script>
+    <script src="https://ecommerce.merchantware.net/v1/CayanCheckout.js" type="text/javascript"></script>
 </head>
 <body>
 
@@ -155,7 +155,7 @@
     
 <script>
     // set credentials to enable use of the API.
-    Cayan.setWebApiKey("pstest;22222222;22222-22222-22222-22222-22222");
+    CayanCheckout.setWebApiKey("8Z0E5CGYTMDJIRK7");
 
     function clearTokenMessageContainer(tokenMessageContainer) {
         tokenMessageContainer.removeClass('alert-danger');
@@ -196,7 +196,7 @@
     // create a submit action handler on the payment form, which calls CreateToken
     $("#SubmitButton").click(function (ev) {
         toggleForm();
-        Cayan.createPaymentToken({ success: HandleTokenResponse, error: HandleErrorResponse });
+        CayanCheckout.createPaymentToken({ success: HandleTokenResponse, error: HandleErrorResponse });
 
         // AJAX SOAP request here
 
