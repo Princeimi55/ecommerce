cdbfd749d030fed24b361dde734d0c3b2036d5f1
Lukasz Gawel <LGawel@cayan.com>
2016-07-29 15:29:20 -0400

    Adding CayanCheckoutSample

---
+++ examples/dotnet/CayanCheckoutSample/CayanCheckoutSample/CheckoutPage.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CayanCheckoutSample.MWareCredit;
+
+namespace CayanCheckoutSample
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        private string Amount = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Amount = this.Request.QueryString["Amount"];
+            this.ShoppingCartAmount.Text = "$" + Amount;
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+
+            var token = this.TokenHolder.Text;
+
+            using (var service = new CreditSoapClient())
+            {
+
+                var response = service.SaleVault(
+                    //Replace "YOUR MERCHANTNAME HERE" with provided merchantName
+                    merchantName: "YOUR MERCHANTNAME HERE",
+                    //Replace "YOUR SITEID HERE" with provided siteID
+                    merchantSiteId: "YOUR SITEID HERE",
+                    //Replace "YOUR KEY HERE" with provided key
+                    merchantKey: "YOUR KEY HERE",
+                    invoiceNumber: "123",
+                    amount: this.Amount,
+                    vaultToken: token,
+                    forceDuplicate: "false",
+                    registerNumber: "123",
+                    merchantTransactionId: "1234");
+
+                if (response.ApprovalStatus.Equals("approved", StringComparison.OrdinalIgnoreCase))
+                {
+                    this.CheckoutPlaceHolder.Visible = false;
+                }
+
+                var error = response.ErrorMessage;
+
+                this.ResponseMessage.Text = response.ApprovalStatus;
+                this.ReferenceNumber.Text = response.Token;
+
+                this.TokenHolder.Text = string.Empty;
+                this.ResultsPlaceHolder.Visible = true;
+            }
+
+        }
+    }
+
+}
---
+++ examples/dotnet/CayanCheckoutSample/CayanCheckoutSample/Images/README.txt
@@ -0,0 +1,3 @@
+Icon images created by Popcic, and used unmodified from his "Clothing and accessories" collection: https://www.iconfinder.com/popcic
+
+All artwork licensed under the Creative Commons Attribution 3.0 Unported License (CC BY 3.0): https://creativecommons.org/licenses/by/3.0/
